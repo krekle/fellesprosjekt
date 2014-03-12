e055b0553987f0424fac3a6cee0cb119839b025f
Krekle <kristian.ekle@gmail.com>
2014-03-12 15:44:48 +0100

    Singleton frameword for user

---
+++ kalender/src/gruppe9/kalender/user/Bruker.java
@@ -0,0 +1,24 @@
+package gruppe9.kalender.user;
+
+
+public class Bruker {
+	
+	//Her vil vi typisk ha ArrayList<Avtaler>, ArrayList<Varsler> osv, osv
+	
+	//field for å holde styr på instansen av Bruker:
+	private static Bruker instance = null;
+	
+	private Bruker() {
+		// private constructor for singleton-pattern
+	}
+
+	public static Bruker getInstance() {
+		if (instance == null) {
+			instance = new Bruker();
+		}
+		return instance;
+	}
+
+	//For å bruke metoder fra bruker kall Bruker.getInstance().*
+	//* = any method, med dette pattern så kan instansen av denne klassen nåes fra hvor som helst.
+}

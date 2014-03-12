28f1748e610b5cb5b95c30b8158e7f2c8cddeb3c
kriekl <kristian.ekle@gmail.com>
2014-03-12 14:43:16 +0100

    Backend stage 2

--- backend/insert.py
+++ backend/insert.py
@@ -2,10 +2,52 @@ import dataset
 
 db = dataset.connect('mysql://bruker:passord@localhost/db2_gr9')
 
-def avtale(d):
-  avtale = db['Avtale']
+def create_avtale(d):
+  meeting = db['Avtale']
+  maker = db['SkaperAv']
+  room = db['TarPlassI']
   try:
-    avtale.insert(d)
+    meet = {d['start'], d['slutt'], d['beskrivelse'], d['varighet'], d['sted']} #sist endret, opprettet <-- SQL now()?
+    avtale.insert(meet)
+    print("Avtale: Success")
+    # Hent avtale_id
+    
+    avtale_id = db.query(
+    person_maker = {d['skaper']} # + avtale_id
+    maker.insert(person_maker)
+    print("Skaper: Success")
+
+    in_room = {d['romid'], d['start'], d['slutt']} # + avtale_id
+    room.insert(in_room)
+    print('Rom: Success')
+
     return True
   except:
+    print("Error")
     return False
+
+def add_people(people):
+  meeting_id = people.pop('avtale_id')
+  for k in people:
+
+
+def create_Person(d):
+  person = db['Person']
+  try:
+    person.insert(d)
+    print("Success")
+    return True
+  except:
+    print("False")
+    return False
+
+def create_varsel():
+  #todo
+  return
+
+def delete_avtale(avtale_id):
+  db['Avtale'].delete(AvtaleID = avtale_id)
+  #Må delete alle entries fra deltagendeI
+  return True
+
+delete_avtale(2)
--- backend/update.py
+++ backend/update.py
@@ -1,6 +1,35 @@
 import dataset
+import datetime
 
 db = dataset.connect('mysql://bruker:passord@localhost/db2_gr9')
 
+#Tested and works
+def add_to_avtale(avtaleid, personid):
+  try:
+    DeltarIData = db['DeltagendeI']
+    DeltarIData.insert(dict(Status='Ikke Svart', SistSett=datetime.datetime.now(), Person_Ansattnummer=personid, Avtale_AvtaleID=avtaleid))
+    print("Success")
+    return True
+  except:
+    print("Error")
+    return False
 
-avtale = db['avtale']
+#Tested and works
+def update_person(d):
+  try:
+    db['Person'].update(d,['Ansattnummer'])
+    print("Success")
+    return True
+  except:
+    print("Error")
+    return False
+
+#Tested and works
+def update_avtale(d):
+  try:
+    db['Avtale'].update(d, ['AvtaleID'])
+    print("Success")
+    return True
+  except:
+    print("Error")
+    return False

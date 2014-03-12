4aa61ed97cd73ab1438122a00b82b19a645d7585
andersob <andersobjornoy@gmail.com>
2014-03-12 16:55:57 +0100

    Started making testinstances
    stated making testinstances, and added some constructors to make it
    easier

---
+++ kalender/src/gruppe9/kalender/model/ListOfTestmodels.java
@@ -0,0 +1,21 @@
+package gruppe9.kalender.model;
+
+//class that makes some test instances
+
+public class ListOfTestmodels {
+
+	
+	
+	//some person test instances
+	
+	public Person person0 = new Person(0,"Test person0",22225555,"Trondheim Sentrum","person0@domain.com");
+	public Person person1 = new Person(1, "Test person1", 291827291, "Ntnu P15", "person1@domain.com");
+	public Person person2 = new Person(2, "Test person2", 28364927, "Høyskoleringen 3", "person2@domain.com");
+	public Person person3 = new Person(3, "Test person3", 394749472, "Your mamas' home", "person3@domain.com");
+	
+	//some meeting test instances
+	
+	
+	public Group group1 = new Group("Gruppe1");
+	
+}

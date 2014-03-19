f0f2609319112c8b18761e7054ecdf1d42b9325f
Fredrik Berg <fredrik_c_berg@hotmail.com>
2014-03-19 17:31:10 +0100

    aaa
    aaa

--- kalender/src/gruppe9/kalender/frontend/Edit_Avtale.java
+++ kalender/src/gruppe9/kalender/frontend/Edit_Avtale.java
@@ -82,7 +82,7 @@ public class Edit_Avtale extends javax.swing.JFrame implements ApiCaller {
 		}
 		else if (response.getCode().equals("200") && complete == true) {
 			id = response.getSimpleResponse("avtaleid");
-			//Legg til deltakere
+			System.out.println(response.getSimpleResponse("avtaleid"));
 			String csv = "";
 			String csvS = "";
 			for (Object o : ((DefaultListModel) person_list.getModel()).toArray()) {
@@ -645,8 +645,8 @@ private void lagre_buttonActionPerformed(java.awt.event.ActionEvent evt) {
 	meeting.setCreator(Bruker.getInstance().getUser().getId());
 	meeting.setDescription(beskrivelse_textfield.getText());
 	meeting.setName(avtalenavn_textfield.getText());
-	meeting.setStart(start);
-	meeting.setEnd(slutt);
+	meeting.setStart(toDateTime(date_textfield.getText(),start_textfield.getText()));
+	meeting.setEnd(toDateTime(date_textfield.getText(), slutt_textfield.getText()));
 	
 	if (rom_list.getSelectedValue() != null) {
 		room = (Room) rom_list.getSelectedValue();
@@ -662,7 +662,7 @@ private void lagre_buttonActionPerformed(java.awt.event.ActionEvent evt) {
 		list.add(person);
 	}
 	meeting.setParticipants(list);
-
+	System.out.print(meeting.getId());
 	if (edit) {
 		complete = true;
 		Database.updateMeeting(this, meeting);
